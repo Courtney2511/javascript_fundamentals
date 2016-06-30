4a9c0be54abc4dab53287d17926783d7228a74a9
Courtney Noonan <courtneynoonan@me.com>
2016-06-30 09:23:27 -0400

    completing arrays assignment

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -7,4 +7,28 @@
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
 
+var topChoices = ["tacos", "sushi", "cats", "music", "sleeping"];
+for (var i = 0; topChoices[i]; i++) {
+  console.log("My #"+ (i+1) + " choice is " + topChoices[i]);
+}
 
+var topChoices = ["tacos", "sushi", "cats", "music", "sleeping"];
+
+
+
+for (var i = 0; topChoices[i]; i++) {
+  var position = (i + 1)
+  var suffix
+    if( position = 1){
+      suffix = "st"
+    }else if( position = 2){
+      suffix = "nd"
+    }else if( position = 3){
+      suffix = "rd"
+    }else if( position = 4){
+      suffix = "th"
+    }else{
+      suffix = "th"
+    }
+  console.log("My " + (i+1) + suffix + " choice is " + topChoices[i]);
+}
