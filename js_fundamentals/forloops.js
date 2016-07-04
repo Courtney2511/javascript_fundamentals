957d09290924f247e9c7de94b866800d1ab40eac
Courtney Noonan <courtneynoonan@me.com>
2016-07-04 09:57:42 -0400

    completing javascript assignment

--- js_fundamentals/forloops.js
+++ js_fundamentals/forloops.js
@@ -2,16 +2,55 @@
 
 // EXERCISE: The even/odd reporter
 
-// Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+// Write a for loop that will iterate from 0 to 20. For each iteration,
+// it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+
+for (i = 0; i < 21; i++){
+  if( ( i % 2 ) == 0 ){
+    console.log(i + " is even");
+  }
+  else{
+    console.log(i + " is odd");
+  }
+}
 
 // EXERCISE: Multiplication Tables
 
-// Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+// Write a for loop that will iterate from 0 to 10. For each iteration of the
+//for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
 
 // Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
 
+for (i = 0; i < 11; i++){
+  result = i * 9
+  console.log( i + " * 9 = " + result )
+}
+
+for (i = 0; i < 11; i++){
+  for (n = 0; n < 11; n++)
+  result = i * n
+  console.log( i + " * " + n + result )
+}
+
 // EXERCISE: The Grade Assigner
 
-// Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
+// Check the results of assignGrade function from the conditionals exercise
+//for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
 
+function assignGrade(score){
+  if( (score < 100) && (score > 79) ){
+    return "A";
+  }else if( (score < 80) && (score > 69) ){
+    return "B";
+  }else if( (score < 70) && (score > 59) ){
+    return "C";
+  }else if( (score < 60) && (score > 49) ){
+    return "D";
+  }else{
+    return "F";
+  }
+}
 
+  for (i = 60; i < 101; i++){
+    console.log("For " + i + ", you got a " + assignGrade(i));
+  }
--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -4,7 +4,8 @@
 
 // Never forget another recipe!
 
-// Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+// Create an object to hold information on your favorite recipe. It should have
+// properties for title (a string), servings (a number), and ingredients (an array of strings).
 // On separate lines (one console.log statement for each), log the recipe information so it looks like:
 // Mole
 // Serves: 2
@@ -13,20 +14,78 @@
 // cumin
 // cocoa
 
+var recipe = {
+  title: "Mole",
+  serves: 2,
+  ingredients: ["cinnamon", "cumin", "coca"],
+};
+
+console.log(recipe.title);
+console.log("Serves: " + recipe.serves);
+console.log(recipe.ingredients[0]);
+console.log(recipe.ingredients[1]);
+console.log(recipe.ingredients[2]);
+
+
 // EXERCISE: The Reading List
 
 // Keep track of which books you read and which books you want to read!
 
-// Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
-// Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
-// Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+// Create an array of objects, where each object describes a book and has properties
+// for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// Iterate through the array of books. For each book, log the book title and book author like
+// so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet or not.
+// If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not,
+// log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+var myBooks = [
+  {title: "Blink",
+   author: "Malcolm Gladwell",
+   readYet: true},
+  {title: "Hamlet",
+    author: "William Shakespeare",
+    readYet: true},
+  {title: "Catcher in the Rye",
+   author: "JD Salinger",
+   readYet: false},
+  {title: "Rails Tutorial",
+    author: "Michael Hartl",
+    readYet: false},
+  {title: "Not That Kind of Girl",
+    author: "Lena Dunham",
+    readYet: true}
+]
+
+for (i = 0; i < myBooks.length; i++) {
+  var myBook = myBooks[i];
+
+  if( myBook.readYet == true ) {
+    console.log("You have already read " + myBook.title + ", by " + myBook.author)
+  }
+  else{
+    console.log("You have not read " + myBook.title + ", by " + myBook.author)
+  }
+}
 
 // EXERCISE: The Movie Database
 
 // It's like IMDB, but much much smaller!
 
-// Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
-// Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
+// Create an object to store the following information about your favorite movie: title (a string),
+// duration (a number), and stars (an array of strings).
+// Create a function to print out the movie information like so:
+// "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
 
 
+var movie = {
+  title: "The Last Unicorn",
+  duration: 84,
+  stars: {1: "Jeff Bridges", 2: "Mia Farrow", 3: "Angela Lansbury"}
+}
 
+function movieDetails(movie) {
+  console.log(movie.title + " lasts for " + movie.duration + " minutes. Stars: " + movie.stars[0] + ", " + movie.stars[1] + ", " + movie.stars[2] + ".")
+}
+
+movieDetails(movie);
